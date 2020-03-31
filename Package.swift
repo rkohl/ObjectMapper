b2b1d3cb19e571612ef1979f4bbf44cd627678be
Ryan Kohl <kohldevelopmentgroup@icloud.com>
2020-03-31 00:38:15 -0500

    Simplifying the folder structure to avoid double-linking of the same classes. Updated to Swift 5.

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.2
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 import PackageDescription
 
 let package = Package(
-    name: "ObjectMapper"
+    name: "ObjectMapper",
+    targets: [
+        .target(
+            name: "ObjectMapper",
+            path: "Sources"
+        ),
+        .testTarget(
+            name: "ObjectMapperTests",
+            dependencies: ["ObjectMapper"])
+    ],
+    swiftLanguageVersions: [.v5]
 )
